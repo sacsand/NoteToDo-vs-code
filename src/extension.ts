3770066dafcfb69c02bdd06f2dbb7e0ae81f140c
Sachintha Sandaruvan <c4c5nf3@gmail.com>
2024-09-22 19:41:17 +0800

    Add icon + Set up main dashboard icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,53 @@
-
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 import * as vscode from 'vscode';
 import { TodoPanel } from './view/todoPanel';
 
+// Create a basic TreeItem for our view
+class TodoTreeItem extends vscode.TreeItem {
+	constructor(
+		public readonly label: string,
+		public readonly command?: vscode.Command
+	) {
+		super(label);
+	}
+}
+
+// Create a TreeDataProvider for the view
+class TodoTreeDataProvider implements vscode.TreeDataProvider<TodoTreeItem> {
+	private _onDidChangeTreeData: vscode.EventEmitter<TodoTreeItem | undefined | void> = new vscode.EventEmitter<TodoTreeItem | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<TodoTreeItem | undefined | void> = this._onDidChangeTreeData.event;
+
+	getTreeItem(element: TodoTreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): TodoTreeItem[] {
+		return [
+			new TodoTreeItem('Open To-Do & Markdown Panel', {
+				command: 'extension.showTodoPanel',
+				title: 'Open To-Do & Markdown Panel'
+			})
+		];
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+}
+
+// This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "todolist" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('todolist.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TodoList!');
+	// Register command to show the TodoPanel
+	const disposablePanel = vscode.commands.registerCommand('extension.showTodoPanel', () => {
+		TodoPanel.createOrShow(context);
 	});
 
-	// Register a command to show the TodoPanel
-	let disposablePanel = vscode.commands.registerCommand('extension.showTodoPanel', () => {
-		const extensionUri = context.extensionUri; // Get the extension URI
-		TodoPanel.createOrShow(context); // Pass the extension URI
-	});
+	// Register the TreeDataProvider for the custom sidebar view
+	const todoTreeDataProvider = new TodoTreeDataProvider();
+	vscode.window.registerTreeDataProvider('todoView', todoTreeDataProvider);
 
-	
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposablePanel);
 }
 
